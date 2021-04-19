59ac512abee939f3d0caf40b93049c1b72b0e01c
Susan Ashmore <scashmorecitcom@gmail.com>
2021-04-19 17:03:46 -0400

    added login btn and ref to sign up

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import {
     Label,
     Input,
-    Select,
-    Radio,
     Checkbox,
 } from '@rebass/forms';
 import {
@@ -16,53 +14,58 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Login() {
     return (
-        <div className="container">
-            <Box
-                className="formCard"
-                as='form'
-                onSubmit={e => e.preventDefault()}
-                py={3}>
-                <Flex mx={-2} mb={3}>
-                    <Box width={1 / 2} px={3}>
-                        <h1>Sign In</h1>
-                        <Label>
-                            <div className="form">
-                                <FontAwesomeIcon icon="user" />
-                                {' '}
+            <div className="container">
+                <Box
+                    className="formCard"
+                    as='form'
+                    onSubmit={e => e.preventDefault()}
+                    py={3}>
+                    <Flex mx={-2} mb={3}>
+                        <Box width={1 / 2} px={3}>
+                            <h1>Sign In</h1>
+                            <Label>
+                                <div className="form">
+                                    <FontAwesomeIcon icon="user" />
+                                    {' '}
                                  Username
                              </div>
-                        </Label>
-                        <Input
-                            id='username'
-                            name='username'
-                            placeholder='username'
-                        />
-                    </Box>
-                    <Box width={1 / 2} px={3}>
-                        <Label>
-                            <div className="form">
-                                <FontAwesomeIcon icon="key" />
-                                {' '}
+                            </Label>
+                            <Input
+                                id='username'
+                                name='username'
+                                placeholder='username'
+                            />
+                        </Box>
+                        <Box width={1 / 2} px={3}>
+                            <Label>
+                                <div className="form">
+                                    <FontAwesomeIcon icon="key" />
+                                    {' '}
                                  Password
                              </div>
-                        </Label>
-                        <Input
-                            id='password'
-                            name='password'
-                            placeholder='password'
-                        />
-                    </Box>
-                    <Label width={[1 / 2, 1 / 4]} p={2}>
-                        <Checkbox
-                            id='remember'
-                            name='remember'
-                        />
-      I am not a Robot
-    </Label>
-                </Flex>
-            </Box>
+                            </Label>
+                            <Input
+                                id='password'
+                                name='password'
+                                placeholder='password'
+                            />
+                        </Box>
+                        <Label width={[1 / 2, 1 / 4]} p={2}>
+                            <Checkbox
+                                id='auth'
+                                name='auth'
+                            />
+                             I am not a Robot
+                         </Label>
+                    </Flex>
+                    <div className="btns">
+                        <Button type="submit">Login</Button>
+                        <Label>Don't have an account?</Label>
+                        <a className="redirect" href="/signup">Sign up here!</a>
+                    </div>
+                </Box>
 
-        </div>
+            </div>
     )
 }
 
