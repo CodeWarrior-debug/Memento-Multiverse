505147ba8a9e0bb12530718577613bf447a9ef30
Joshua Wallace <j.wallace21112@gmail.com>
2021-04-19 14:07:20 -0400

    finished cartController

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -31,10 +31,11 @@ const cart = {
                 });
             } else {
                 res.sendStatus(403);
-                console.log("cartController: User is not logged in");
+                console.log("User is not logged in");
             }
     }, 
 
+    // Deleting an item out of user cart
     deleteOne: function (req, res) {
         console.log("Requested item from delete", req);
         if (req.user) {
@@ -42,7 +43,48 @@ const cart = {
 
             const itemDelete = req.params.id;
             db.Cart.deleteOne(itemDelete)
-            .then(cartdb)
+            .then(cartdb => {
+                console.log("Item has been deleted from cart");
+                res.json(cartdb);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+        else {
+            res.sendStatus(403);
+            console.log('User is not logged in')
+        }
+    },
+
+    // Updating user cart
+    updateONe: function (req, res) {
+        if (req.user) {
+            db.cart.updateONe({_id: req.body.id}, {orderQty: req.body.quantity})
+            .then((updated) => {
+                console.log('Your cart has been updated', updated);
+                res.json(updated);
+            })
+        }
+    },
+
+    // Creating the order when the customer is ready to buy
+    createOrder = function (req, res) {
+        if (req, res) {
+            db.Cart.find({user: req.user.id})
+            .then((res) => {
+                const order = res.map((element) => {
+                    return {item: element.item, orderQty: element.orderQty}
+                })
+                console.log('Here is your order');
+                console.log('Your total is', typeof req.body);
+                return db.Order.create({user: req.user.id,
+                    items: order,
+                    total: req.body.total})
+            }).then(res => {
+                console.log('Your order has been created! Thank you for shopping in the Multiverse!');
+                return db.Cart.remove({user: req.user.id})
+            }) 
         }
     }
 }
