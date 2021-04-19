fc5fb67e32c018fe8fb3991b74395d4f6034dcf1
Nicholas Marrone <nmarrone1997@gmail.com>
2021-04-19 13:14:40 -0400

    Building out Card component to map over image, title, price, in stock, numSold, start review, description

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,20 +1,44 @@
 import React from "react";
 
-function index() {
-  return (
-    <div>
-      {/* product image */}
-      {/* product name */}
-      {/* product price */}
-      {/* product in Stock */}
-      {/* product # Sold */}
-      {/* product review */}
-      {/* product description */}
-      {/* qty chosen to add to buy/cart */}
-      {/* buy button */}
-      {/* add to cart button */}
-    </div>
-  );
-}
+import {
+  Box,
+  Card,
+  Image,
+  Heading,
+  Text
+} from 'rebass'
 
-export default index;
+const productCard = () => ({
+  image,
+  title,
+  description
+}) =>
+  <Box width={256}>
+    <Card
+      sx={{
+        p: 1,
+        borderRadius: 2,
+        boxShadow: '0 0 16px rgba(0, 0, 0, .25)',
+      }}>
+      <Image src={image} />
+      <Box px={2}>
+        <Heading as='h3'>
+          {title}
+        </Heading>
+        <Text fontSize={0}>
+          {description}
+        </Text>
+        {/* <div>
+
+        </div>
+        <Text fontSize={0}>
+          {quantityNumber}
+        </Text>
+        <Text fontSize={0}>
+          {stockNumber}
+        </Text> */}
+      </Box>
+    </Card>
+  </Box>
+
+export default productCard;
