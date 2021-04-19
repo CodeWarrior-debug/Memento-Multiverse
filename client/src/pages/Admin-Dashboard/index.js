5153eb67d0f059fc5822858a2c99df3133f287de
Nicholas Marrone <nmarrone1997@gmail.com>
2021-04-19 11:34:19 -0400

    Commenting pages

--- client/src/pages/Admin-Dashboard/index.js
+++ client/src/pages/Admin-Dashboard/index.js
@@ -3,7 +3,8 @@ import React from 'react'
 function AdminDasboard() {
     return (
         <div>
-            
+            {/* Admin header */}
+            {/* Toggle buttons to display info to the admin */}
         </div>
     )
 }
--- client/src/pages/Shop/index.js
+++ client/src/pages/Shop/index.js
@@ -3,7 +3,9 @@ import React from 'react'
 function index() {
     return (
         <div>
-            
+            {/* Header with Nav Bar */}
+            {/* Vertical Checkbox on Left for Sprites */}
+            {/* Row of 4 Cards with Product image, price, in stock, sold, star review, description and buy now button */}
         </div>
     )
 }
