6690d3355b9a273b4649cdea44c2d9d90105b38c
Joshua Wallace <j.wallace21112@gmail.com>
2021-04-20 13:55:10 -0400

    fixed indexRoute

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,8 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-const loginRoutes = require("./loginRoutes");
-const logoutRoutes = require('./logoutRoutes');
-const shopRoutes = require('./shopRoutes');
-const signupRoutes = require('./signupRoutes');
-const adminRoutes = require('./adminRoutes');
-const consumerRoutes = require('./consumerRoutes');
-const userAuth = require('./userAuth');
-const orderRoutes = requre('./orderRoutes');
-=======
-const loginRoutes = require("./loginRoutes.js");
-const logoutRoutes = require('./logoutRoutes.js');
-const shopRoutes = require('./shopRoutes.js');
-const signupRoutes = require('./signupRoutes.js');
-const adminRoutes = require('./adminRoutes.js');
-const consumerRoutes = require('./consumerRoutes.js');
-const userAuth = require('./userAuth.js');
-const orderRoutes = requre('./orderRoutes.js');
->>>>>>> main
-const apiRoutes = require('./api')
+const appRoutes = require('./appRoutes');
+const userAuth = require('./userAuth')
 
-router.use('/', loginRoutes);
-router.use('/', logoutRoutes);
-router.use('/', shopRoutes);
-router.use('/', signupRoutes);
-router.use('/', adminRoutes);
-router.use('/', consumerRoutes);
 router.use('/', userAuth);
-router.use('/', orderRoutes);
-router.use('/', apiRoutes);
+router.use('/', appRoutes);
 
 module.exports = router;
