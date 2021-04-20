5b879bd19b27a9727e55ad779ec77ccd9f458b83
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-20 12:22:47 -0400

    adding another function with comments to number range

--- client/src/components/Admin-Table/index.js
+++ client/src/components/Admin-Table/index.js
@@ -173,3 +173,19 @@ function SliderColumnFilter({
         </>
     )
 }
+
+// This is a custom UI for out 'between' or number range
+// filter, It uses two number boxes and filters rows to
+// ones that have values between the two
+function NumberRangeColumnFilter({
+    column: { filterValue = [], preFilteredRows, setFilter, id },
+}) {
+    const [min, max] = React.useMemo(() => {
+        let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+        let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+        preFilteredRows.forEach(row => {
+            min = Math.min(row.values[id], min)
+            max = Math.max(row.values[id], max)
+        })
+    })
+}
