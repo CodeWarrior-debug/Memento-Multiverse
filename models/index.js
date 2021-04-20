cb161599cf8f3b0231f977fa0bb68e4476e4bbc5
James Jordan <jpjordan1987@gmail.com>
2021-04-20 10:44:28 -0400

    index model relationships

--- models/index.js
+++ models/index.js
@@ -3,6 +3,11 @@ const Transaction = require('./Transaction');
 const User = require('./User');
 const Franchise = require('./Franchise');
 
+Franchise.hasMany(Product);
+Product.belongsTo(Franchise);
+
+User.hasMany(Transaction);
+Transaction.belongsTo(User);
 
 
 module.exports = { User, Transaction, Product, Franchise };
