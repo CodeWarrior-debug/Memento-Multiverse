a3a8c8d40e555c6c57f80eb452caa5f7559cf2e4
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-20 13:35:35 -0400

    added Josh's code into the table

--- client/src/components/Admin-Table/index.js
+++ client/src/components/Admin-Table/index.js
@@ -227,3 +227,179 @@ function NumberRangeColumnFilter({
         </div>
     )
 }
+
+function fuzzyTextFilterFn(rows, id, filterValue) {
+    return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+}
+
+//remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = val => !val
+
+//pass the updateMyData and the skipReset option
+function Table({ columns, data, updateMyData, skipReset }) {
+    const filterTypes = React.useMemo(
+        () => ({
+            // Add a new fuzzyTextFilterFn filter type.
+            fuzzyText: fuzzyTextFilterFn,
+            text: (rows, id, filterValue) => {
+                return rows.filter(row => {
+                    const rowValue = row.values[id]
+                    return rowValue !== undefined
+                        ? String(rowValue)
+                            .toLowerCase()
+                            .startsWith(String(filterValue).toLowerCase())
+                        : true
+                })
+            },
+        }),
+        []
+    )
+
+    const defaultColumn = React.useMemo(
+        () => ({
+            Filter: DefaultColumnFilter,
+            Cell: EditableCell,
+        }),
+        []
+    )
+
+    //building the UI
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        page,
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        state: {
+            pageIndex,
+            pageSize,
+            sortBy,
+            groupBy,
+            expanded,
+            filters,
+            selectedRowIds,
+        },
+    } = useTable(
+        {
+            columns,
+            data,
+            defaultColumn,
+            filterTypes,
+            updateMyData,
+            autoResetPage: !skipReset,
+            autoResetSelectedRows: !skipReset,
+            disableMultiSort: true,
+        },
+        useFilters,
+        useGroupBy,
+        useSortBy,
+        useExpanded,
+        usePagination,
+        useRowSelect,
+        hooks => {
+            hooks.visibleColumns.push(columns => {
+                return [
+                    {
+                        id: 'selection',
+                        groupByBoundary: true,
+                        Header: ({ getToggleAllRowsSelectedProps }) => (
+                            <div>
+                                <IndeterminateCheckbox {...getToggleAllRowsSelectedProps()} />
+                            </div>
+                        ),
+                        Cell: ({ row }) => (
+                            <div>
+                                <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
+                            </div>
+                        ),
+                    },
+                    ...columns,
+                ]
+            })
+        }
+    )
+
+    // Rendering the UI for the table
+    return (
+    <>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps()}>
+                                    <div>
+                                        {column.canGroupBy ? (
+                                            <span {...column.getGroupByToggleProps()}>
+                                                {column.isGrouped ? '🛑 ' : '👊 '}
+                                            </span>
+                                        ) : null}
+                                        <span {...column.getSortByToggleProps()}>
+                                            {column.render('Header')}
+                                            {/* Add a sort direction indicator */}
+                                            {column.isSorted
+                                                ? column.isSortedDesc
+                                                    ? ' 🔽'
+                                                    : ' 🔼'
+                                                : ''}
+                                        </span>
+                                    </div>
+                                    {/* Render the columns filter UI */}
+                                    <div>{column.canFilter ? column.render('Filter') : null}</div>
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {page.map(row => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return (
+                                        <td {...cell.getCellProps()}>
+                                            {cell.isGrouped ? (
+                                                <>
+                                                    <span {...row.getToggleRowExpandedProps()}>
+                                                        {row.isExpanded ? '👇' : '👉'}
+                                                    </span>{' '}
+                                                    {cell.render('Cell', { editable: false })} (
+                                                    {row.subRows.length})
+                                                </>
+                                            ) : cell.isAggregated ? (
+                                                cell.render('Aggregated')
+                                            ) : cell.isPlaceholder ? null : (
+                                                cell.render('Cell', { editable: true })
+                                            )}
+                                        </td>
+                                    )
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            {/*
+      */}
+            <div className="pagination">
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </button>{' '}
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    {'<'}
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                </button>{' '}
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {'>>'}
+                </button>{' '}
