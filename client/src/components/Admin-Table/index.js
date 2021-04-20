9afcb497507739966761d380c696a43df65506e8
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-20 12:20:02 -0400

    added the min and max functionality

--- client/src/components/Admin-Table/index.js
+++ client/src/components/Admin-Table/index.js
@@ -151,5 +151,25 @@ function SliderColumnFilter({
     const [min, max] = React.useMemo(() => {
         let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
         let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
-    })
+        preFilteredRows.forEach(row => {
+            min = Math.min(row.values[id], min)
+            max = Math.max(row.values[id], max)
+        })
+        return [min, max]
+    }, [id, preFilteredRows])
+
+    return (
+        <>
+            <input
+                type="range"
+                min={min}
+                max={max}
+                value={filteredValue || min}
+                onChange={e => {
+                    setFilter(parseInt(e.target.value, 10))
+                }}
+            />
+            <button onclick={() => setFilter(undefined)}>Off</button>
+        </>
+    )
 }
