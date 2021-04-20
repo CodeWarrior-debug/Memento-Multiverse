3a748709875a2d9df99a4154811feb13d0bd0474
Susan Ashmore <scashmorecitcom@gmail.com>
2021-04-20 09:47:49 -0400

    admin tabs

--- client/src/pages/Admin-Dashboard/index.js
+++ client/src/pages/Admin-Dashboard/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {
     Box,
+    Text,
+    Link,
+    Flex,
     Button,
 } from 'rebass';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -8,7 +11,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function Admin() {
     return (
         <div>
-
+            <Flex
+                px={2}
+                color='white'
+                bg='black'
+                alignItems='center'>
+                <Box />
+                <Link variant='nav' href='#!'>
+                    Invintory
+                </Link>
+                <Link variant='nav' href='#!'>
+                    Sales by Francise
+                </Link>
+                <Link variant='nav' href='#!'>
+                    All Sales
+                </Link>
+                <Link variant='nav' href='#!'>
+                    Expenses
+                </Link>
+                <Link variant='nav' href='#!'>
+                    Net Revenu
+                </Link>
+            </Flex>
         </div>
     )
 }
