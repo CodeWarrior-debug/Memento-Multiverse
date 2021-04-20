0db0648e67154839bc778b687f1dacb1eba52697
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-20 12:16:09 -0400

    adding more content to the table

--- client/src/components/Admin-Table/index.js
+++ client/src/components/Admin-Table/index.js
@@ -120,4 +120,25 @@ function SelectColumnFilter({
         })
         return [...options.values()]
     }, [id, preFilteredRows])
+
+    // Render a multi-select box
+    return (
+        <select
+            value={filterValue}
+            onChange={e => {
+                setFilter(e.target.value || undefined)
+            }}
+        >
+            <option value=""></option>
+            {options.map((option, i) => (
+                <option key={i} value={option}>
+                    {option}
+                </option>
+            ))}
+        </select>
+    )
 }
+
+// This is a custom filter UI that uses a
+// slider to set the filter value between a column's
+// min and max values
