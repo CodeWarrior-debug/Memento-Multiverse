bfddfbd3413fc96cedb43892fe84bcc192044492
Joshua Wallace <j.wallace21112@gmail.com>
2021-04-20 13:07:14 -0400

    admin-table01

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
-const loginRoutes = require("./loginRoutes");
-const logoutRoutes = require('./logoutRoutes');
-const shopRoutes = require('./shopRoutes');
-const signupRoutes = require('./signupRoutes');
-const adminRoutes = require('./adminRoutes');
-const consumerRoutes = require('./consumerRoutes');
-const userAuth = require('./userAuth');
-const orderRoutes = requre('./orderRoutes');
+const loginRoutes = require("./loginRoutes.js");
+const logoutRoutes = require('./logoutRoutes.js');
+const shopRoutes = require('./shopRoutes.js');
+const signupRoutes = require('./signupRoutes.js');
+const adminRoutes = require('./adminRoutes.js');
+const consumerRoutes = require('./consumerRoutes.js');
+const userAuth = require('./userAuth.js');
+const orderRoutes = requre('./orderRoutes.js');
+const apiRoutes = require('./api')
 
 router.use('/', loginRoutes);
 router.use('/', logoutRoutes);
@@ -16,5 +17,6 @@ router.use('/', adminRoutes);
 router.use('/', consumerRoutes);
 router.use('/', userAuth);
 router.use('/', orderRoutes);
+router.use('/', apiRoutes);
 
 module.exports = router;
