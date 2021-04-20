51de3442256d7c1a70b03e40b92bb06a8a2d1401
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-20 10:29:43 -0400

    added Franchise inside models; index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const Product = require('./Product');
 const Transaction = require('./Transaction');
 const User = require('./User');
+const Franchise = require('./Franchise');
 
 
 
-module.exports = { User, Transaction, Product };
+module.exports = { User, Transaction, Product, Franchise };
