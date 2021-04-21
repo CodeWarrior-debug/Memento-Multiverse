bbe5d1e07388fd6fe59bc3401a5d918cdd233306
Susan Ashmore <scashmorecitcom@gmail.com>
2021-04-21 10:05:57 -0400

    flex wrapper store

--- client/src/pages/Shop/index.js
+++ client/src/pages/Shop/index.js
@@ -1,26 +1,112 @@
 import React, { useEffect, useState } from 'react'
 import Card from '../../components/Card.js';
+import {
+    Box,
+    Flex,
+    Button
+} from 'rebass';
+import './style.css'
+
 
 function Shop() {
-    const [itemList, setItemList] =useState([]);
-    useEffect(()=>{
-      setItemList([{
-             id: 1,
-            image: "https://imgur.com/9nfklQu.png", 
-             title: "Mega Seed",
-             price: "25", 
-             stock: "340",
-             sold: "127",
-             description: "Brought to you straight form the fruit of mega seed tress from Dimension 35-C. These mega seeds are of the highest quality available. They contain many properties, however, the most well know is their ability to briefly endow  a person with super-intelligence when disolved in the rectum. In addidition, when you buy two or more mega seeds, we will include a specail butt cream for free! Warning: Once the supper-intelligence wears off, the user will lose motor and/or brain funciton for several days."
-         }])
 
-    },[])
+
+    const [itemList, setItemList] = useState([]);
+    useEffect(() => {
+        setItemList([{
+            id: 1,
+            image: "https://imgur.com/9nfklQu.png",
+            title: "Mega Seed",
+            price: "25",
+            stock: "340",
+            sold: "127",
+
+        },
+        {
+            id: 2,
+            image: "https://imgur.com/srBEPdv.png",
+            title: "Jerry's Mind Blowers",
+            price: "Free",
+            stock: "99999",
+            sold: "0",
+
+
+        },
+        {
+            id: 3,
+            image: "https://imgur.com/nT6qzNy.png",
+            title: "Mulan McNugget Sauce",
+            price: "400",
+            stock: "300",
+            sold: "127",
+
+        },
+        {
+            id: 4,
+            image: "https://imgur.com/XdTxABN.png",
+            title: "Plumbus",
+            price: "6.5",
+            stock: "69",
+            sold: "34",
+
+        },
+        {
+            id: 4,
+            image: "https://imgur.com/XdTxABN.png",
+            title: "Plumbus",
+            price: "6.5",
+            stock: "69",
+            sold: "34",
+
+        },
+        {
+            id: 1,
+            image: "https://imgur.com/9nfklQu.png",
+            title: "Mega Seed",
+            price: "25",
+            stock: "340",
+            sold: "127",
+
+        },
+        {
+            id: 2,
+            image: "https://imgur.com/srBEPdv.png",
+            title: "Jerry's Mind Blowers",
+            price: "Free",
+            stock: "99999",
+            sold: "0",
+
+
+        },
+        {
+            id: 3,
+            image: "https://imgur.com/nT6qzNy.png",
+            title: "Mulan McNugget Sauce",
+            price: "400",
+            stock: "300",
+            sold: "127",
+
+        },
+        ])
+
+    }, [])
     return (
-        <div>
+        <div className="container">
+        <Box className="toggel">
+                <Flex>
+                    <Box>
+                        <h1>Hello</h1>
+                    </Box>
+                </Flex>
+            </Box>
+        <Box>
+        <div className="shop">
             {
-                itemList.map((item)=><Card key={item.id} {...item}  />)
+                itemList.map((item) => <Card className="item" key={item.id} {...item} />)
             }
-            
+
+        </div>
+        </Box>
         </div>
     )
 }
