7302013773dff2e0120585e553f8d6a03647e0d7
Joshua Wallace <j.wallace21112@gmail.com>
2021-04-22 12:51:43 -0400

    updating server.js

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const sequelize = require("sequelize");
-const  route  = require("./routes");
 const passport = require("passport");
 const LocalStrategy = require('passport-local').Strategy;
+const routes = require("./routes");
 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(require('express-session')({
+  secret: 'keyboard mouse'
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 // Serve up static assets (usually on heroku)
@@ -14,9 +23,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+
 // Send every request to the React app
 // Define any API routes before this runs
-app.use(route)
+app.use(routes);
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
