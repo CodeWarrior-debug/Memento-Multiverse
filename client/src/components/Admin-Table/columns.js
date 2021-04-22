487f05ff62619d7b03bdb8244b540b327583361b
James Jordan <jpjordan1987@gmail.com>
2021-04-22 16:07:09 -0400

    restarting table from the ground up

---
+++ client/src/components/Admin-Table/columns.js
@@ -0,0 +1,26 @@
+export const COLUMNS = 
+
+[
+{
+    Header: 'ID',
+    accessor: 'id'
+},
+{
+    Header: 'Product_ID',
+    accessor: 'product_id'
+},
+{
+    Header: 'Franchise_ID',
+    accessor: 'franchise_id'
+},
+{
+    Header: 'Quantity',
+    accessor: 'quantity'
+},
+{
+    Header: 'Created At',
+    accessor: 'created_at'
+}
+
+
+]
