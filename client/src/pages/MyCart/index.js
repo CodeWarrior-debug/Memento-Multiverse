8cca0a5132bc5e61227f3901cac6ba34a65bc218
Susan Ashmore <scashmorecitcom@gmail.com>
2021-04-22 12:23:08 -0400

    experimenting

--- client/src/pages/MyCart/index.js
+++ client/src/pages/MyCart/index.js
@@ -5,7 +5,6 @@ import {
     cartLocalization
   } from 'react-shopping-cart';
 
-import 'bootstrap/dist/css/bootstrap.css';
 import "animate.css/animate.min.css";
 import "font-awesome/css/font-awesome.min.css";
 
