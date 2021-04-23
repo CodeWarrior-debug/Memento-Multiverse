60069927bb76bbbdd96983fae0618c6a49554f5d
Susan Ashmore <scashmorecitcom@gmail.com>
2021-04-23 11:07:54 -0400

    save merge

--- client/src/pages/MyCart/index.js
+++ client/src/pages/MyCart/index.js
@@ -51,10 +51,6 @@ const MyCart = () => {
       <input type="submit" value="add" onClick={() => addToCart(product)} />
     </div>
   ));
-<<<<<<< HEAD
-
-=======
->>>>>>> 574deeea6bb1df81059436836b33b58b34b15a1e
   const cartItems = cart.map((product) => (
     <div key={product.id}>
       {`${product.name}: $${product.price}`}
