f7bd88ff1bae81751f4aa6f9f6352da5ab843f18
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-23 10:38:59 -0400

    changed all the functions to arrow functions and added signup to API.js inside the utils folder

--- routes/API/index.js
+++ routes/API/index.js
@@ -3,11 +3,11 @@ const cartRoutes = require('./cartRoutes');  //TODO will this be local storage o
 const franchiseRoutes = require('./franchiseRoutes');
 const productRoutes = require('./productRoutes');
 const transactionRoutes = require('./transactionRoutes');
-// const userAuthRoutes = require('./userAuth');
+const userAuth = require('./userAuth');
 //TODO do we need a User route, that does not consider Authentication?
 
 
-//router.use('/users', userAuth);
+router.use('/users', userAuth);
 router.use('/products', productRoutes);
 router.use('/transactions', transactionRoutes);
 router.use('/cart', cartRoutes); //TODO will this be local storage or DB table?
