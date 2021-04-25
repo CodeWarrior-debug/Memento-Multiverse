86f57ff658955a3fdc87b803567e1de49fa5cfe2
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-25 13:13:39 -0400

    fixed the users again

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -35,7 +35,7 @@ function Header() {
                     {' '}
                     Cart
                 </Link>
-                <Link className="navLink" variant='nav' href='/'>
+                <Link className="navLink" variant='nav' href='/login'>
                     <FontAwesomeIcon icon="sign-out-alt" />
                     {' '}
                     Logout
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -48,7 +48,7 @@ export default {
     },
 
     logOut: () => {
-        return axios.post('/api/uesrs/logout')
+        return axios.post('/api/users/logout')
     },
 
     signUp: (userInfo) => {
