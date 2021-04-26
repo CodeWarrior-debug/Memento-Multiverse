c3fbf7ed933dd7a2e24c93aa60b3f677711811bd
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-26 14:35:04 -0400

    added the previously added things to index.js on mycart '

--- client/src/pages/MyCart/index.js
+++ client/src/pages/MyCart/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import "./style.css";
 import { Button } from "rebass";
 import CartContext from "../../utils/CartContext";
+import API from "../../utils/API";
 
 const MyCart = () => {
   const cart = useContext(CartContext);
@@ -16,6 +17,16 @@ const MyCart = () => {
     setCartTotal(totalVal);
   }, [cart]);
 
+  const handleCheckout = e => {
+    e.preventDefault();
+
+    API.postTransactions
+      .then(() => {})
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
   const removeFromCart = (product) => {
     let hardCopy = [...cart];
     hardCopy = hardCopy.filter((cartItem) => cartItem.id !== product.id);
@@ -27,7 +38,7 @@ const MyCart = () => {
       <h1>My Cart</h1>
       {/* <p>{cartItems}</p> */}
       <h3>Total: ${parseFloat(cartTotal)}</h3>
-      <Button className="btn">Checkout</Button>
+      <Button className="btn" onClick={handleCheckout}>Checkout</Button>
     </div>
   );
 };
